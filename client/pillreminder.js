88f0d904b1d454a2b20e4e66b7bc7548c1a11a0b
Sashank Thupukari <sthupukari@gmail.com>
2014-11-01 06:16:20 -0400

    Login/Logout logic

--- client/pillreminder.js
+++ client/pillreminder.js
@@ -1,32 +1,37 @@
 Patients = new Meteor.Collection('patients');
 
 Router.route('/login', function() {
-
     this.render('login');
 });
-
-Router.route('/dashboard', function() {
-    name: 'dashboard',
-      this.layout('ApplicationLayout');
-
-    this.render('dashboard');
-})
-
 Router.route('/signup', function() {
     name: 'signup',
     this.render('signup');
-})
-
+});
+Router.onBeforeAction(function() {
+    // all properties available in the route function
+    // are also available here such as this.params
+    if (!Meteor.user()) {
+        // if the user is not logged in, render the Login template
+        this.render('login');
+    } else {
+        // otherwise don't hold up the rest of hooks or our route/action function from runnning
+        this.next();
+    }
+}, {
+    except: ['login', 'signup']
+});
+Router.route('/dashboard', function() {
+    name: 'dashboard';
+    this.layout('ApplicationLayout');
+    this.render('dashboard');
+});
 Router.route('/addPatient', function() {
-    name: 'addPatient',
-      this.layout('ApplicationLayout');
-
+    name: 'addPatient';
+    this.layout('ApplicationLayout');
     this.render('addPatient');
 });
-
 if (Meteor.isClient) {
-
-	 Template.login.events = {
+    Template.login.events = {
         'click #signin': function(event) {
             event.preventDefault();
             var username = $('#email').val();
@@ -35,26 +40,23 @@ if (Meteor.isClient) {
                 if (error) {
                     alert(error.reason + 'error');
                 } else {
-                    Router.go('/mynotes');
+                    Router.go('/dashboard');
                     alert('You are now logged in.');
                 }
             });
         }
     };
-
-	Template.signup.events({
-		'click #signup': function(event, template){
-			event.preventDefault();
-
+    Template.signup.events({
+        'click #signup': function(event, template) {
+            event.preventDefault();
             var user = {
                 email: $('#email').val(),
                 password: $('#password').val(),
                 profile: {
-                	firstname: $("#firstname").val(),
-                	lastname: $("#lastname").val()
+                    firstname: $("#firstname").val(),
+                    lastname: $("#lastname").val()
                 }
             };
-
             if (!user.email || !user.password || !user.profile.firstname || !user.profile.lastname) {
                 alert('Please fill in all fields');
             } else {
@@ -80,8 +82,12 @@ if (Meteor.isClient) {
         }
     })
 
+    Template.header.events({
+        'click .logout': function() {
+            Meteor.logout();
+        }
+    });
 }
-
 if (Meteor.isServer) {
     Meteor.startup(function() {
         // code to run on server at startup
