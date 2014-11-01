2799b713898460d2e6795495cc63d68330248ed8
Sashank Thupukari <sthupukari@gmail.com>
2014-11-01 00:21:36 -0400

    User Accounts

--- client/pillreminder.js
+++ client/pillreminder.js
@@ -2,7 +2,7 @@ Router.configure({
     layoutTemplate: 'ApplicationLayout'
 });
 
-Router.route('/', function() {
+Router.route('/login', function() {
     this.render('login');
 });
 
@@ -18,11 +18,48 @@ Router.route('/signup', function() {
 
 if (Meteor.isClient) {
 
-	Template.login.events({
-		'click #signin': function(event, template) {
-			alert("signin");
+	 Template.login.events = {
+        'click #signin': function(event) {
+            event.preventDefault();
+            var username = $('#email').val();
+            var password = $('#password').val();
+            Meteor.loginWithPassword(username, password, function(error) {
+                if (error) {
+                    alert(error.reason + 'error');
+                } else {
+                    Router.go('/mynotes');
+                    alert('You are now logged in.');
+                }
+            });
+        }
+    };
+
+	Template.signup.events({
+		'click #signup': function(event, template){
+			event.preventDefault();
+
+            var user = {
+                email: $('#email').val(),
+                password: $('#password').val(),
+                profile: {
+                	firstname: $("#firstname").val(),
+                	lastname: $("#lastname").val()
+                }
+            };
+
+            if (!user.email || !user.password || !user.profile.firstname || !user.profile.lastname) {
+                alert('Please fill in all fields');
+            } else {
+                Accounts.createUser(user, function(error) {
+                    if (error) {
+                        alert(error.reason + 'error');
+                    } else {
+                        Router.go('/dashboard');
+                    }
+                });
+            }
 		},
-	});
+	})
 
 }
 
